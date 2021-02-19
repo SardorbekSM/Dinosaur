383db99b5ac1a4e7910adee4abc0bd7057fd0d21
Sardorbek <sardorbekmamadaliyevv@gmail.com>
2021-02-20 00:40:25 +0500

    Created a rotation script for an airborne obstacle

---
+++ Assets/Scripts/Enemies/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    private float rotate = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        rotate += Time.fixedDeltaTime * 100;
+        transform.Rotate(0,0, rotate);        
+    }
+}
