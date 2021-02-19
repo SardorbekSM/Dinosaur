5276652114be1f08946f2df1a9cfc31183639a0a
Sardorbek <sardorbekmamadaliyevv@gmail.com>
2021-02-19 23:57:13 +0500

    Some code changes

--- Assets/Scripts/Enemies/Rendomise.cs
+++ Assets/Scripts/Enemies/Rendomise.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rendomise : Movement
+public class Rendomise : MonoBehaviour
 {
     [SerializeField]
     private GameObject Enemy;
@@ -17,8 +17,7 @@ public class Rendomise : Movement
         while (true)
         {
             Instantiate(Enemy, new Vector3(10, -3, 0), Quaternion.Euler(0, 0, Random.Range(0f, 0)));
-            yield return new WaitForSeconds(Random.Range(3f, 5f));
+            yield return new WaitForSeconds(Random.Range(2f, 4f));
         }
-
     }
 }
