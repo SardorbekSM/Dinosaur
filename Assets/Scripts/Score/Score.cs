9041ff8435dd38b080e0d4d18cb539cc80b183fe
Sardorbek <sardorbekmamadaliyevv@gmail.com>
2021-02-20 20:48:12 +0500

    Added score score script, font,ui canvas and ui text.

---
+++ Assets/Scripts/Score/Score.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Score : MonoBehaviour
+{
+    private int score;
+    private Text _scoreText;
+
+    private void Awake()
+    {
+        _scoreText = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        ++score;
+        _scoreText.text = Convert.ToString(score/100);
+    }
+}
