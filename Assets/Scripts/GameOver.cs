d29183e495b4e67e15083c609087200a0926eaee
Sardorbek <sardorbekmamadaliyevv@gmail.com>
2021-02-21 20:32:19 +0500

    added a script for ending the game

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    private bool active = true;
+
+    [SerializeField]
+    protected GameObject canvas;
+
+    private void Awake()
+    {
+        canvas.SetActive(false);
+
+    }
+
+    protected virtual void Hide()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            if (active)
+            {
+                canvas.SetActive(true);
+                active = false;
+            }
+            else
+
+            {
+                canvas.SetActive(false); active = true;
+            }
+
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+        }
+        else
+            Time.timeScale = 1;
+    }
+}
