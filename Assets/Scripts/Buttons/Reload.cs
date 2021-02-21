3d95e902d2bce039e07c766e423ae02fa71d61f8
Sardorbek <sardorbekmamadaliyevv@gmail.com>
2021-02-21 20:29:35 +0500

    Added reload button

---
+++ Assets/Scripts/Buttons/Reload.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Reload : MonoBehaviour
+{
+    public void Reloaded()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+        Time.timeScale = 1;
+    }
+}
