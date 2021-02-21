d3a9b347607b91b863c7553547a36d0857707b61
Sardorbek <sardorbekmamadaliyevv@gmail.com>
2021-02-21 20:35:08 +0500

    removed unneeded commits

--- Assets/Scripts/Enemies/Destroy.cs
+++ Assets/Scripts/Enemies/Destroy.cs
@@ -1,32 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-/*
-public class Destroy : MonoBehaviour
-{
-    [SerializeField]
-    private float _speed;
-
-    protected Vector3 position;
-    private Transform _transform;
-
-    // Start is called before the first frame update
-    private void Start()
-    {
-        _transform = GetComponent<Transform>();
-        position = _transform.position; 
-        Destroy(gameObject, 4f);
-    }
-
-    // Update is called once per frame
-    protected virtual void Move()
-    {
-        position.x += _speed * Time.deltaTime;
-        _transform.position = position;
-    }
-
-}
-*/
 
 public class Destroy : Movement
 {
