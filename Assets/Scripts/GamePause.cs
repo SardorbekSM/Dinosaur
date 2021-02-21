062c8f2d951f47fabd610722e48de6443adcc522
Sardorbek <sardorbekmamadaliyevv@gmail.com>
2021-02-21 20:40:15 +0500

    made a script to pause when clicking a button

---
+++ Assets/Scripts/GamePause.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePause : MonoBehaviour
+{
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+            }
+            else
+                Time.timeScale = 1;
+        }
+    }
+}
