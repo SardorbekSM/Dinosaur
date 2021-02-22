0b48d97af3b81e4f142ef74e632cac458dcc827c
Sardorbek <sardorbekmamadaliyevv@gmail.com>
2021-02-23 00:42:42 +0500

    shortened the code

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -1,35 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GameOver : MonoBehaviour
 {
-    private bool _active = true;
+    private bool _active = false;
 
     [SerializeField]
     protected GameObject canvas;
 
     private void Awake()
     {
-        canvas.SetActive(false);
-
+        canvas.SetActive(_active);
     }
 
     protected virtual void Hide()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-            if (_active)
-            {
-                canvas.SetActive(true);
-                _active = false;
-            }
-            else
+        if (_active)
+        {
+            canvas.SetActive(!(_active = false));
+        }
+        else
+        {
+            canvas.SetActive(!(_active = true));                
+        }
+        Pause();
+    }
 
-            {
-                canvas.SetActive(false);
-                _active = true;
-            }
-
+    private void Pause()
+    {
         if (Time.timeScale == 1)
         {
             Time.timeScale = 0;
