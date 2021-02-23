885fad98c522d8f1d43efa7cdd73b8d2aaead8a1
Sardorbek <sardorbekmamadaliyevv@gmail.com>
2021-02-23 22:57:32 +0500

    fixed name error

---
+++ Assets/Scripts/Enemies/Randomise.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Randomise : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _Enemy;
+
+    [SerializeField]
+    private float _height;
+    
+    void Start()
+    {
+        StartCoroutine(spawn());
+    }
+
+    IEnumerator spawn()
+    {
+        while (true)
+        {
+            Instantiate(_Enemy, new Vector3(10, _height, 0), Quaternion.Euler(0, 0, Random.Range(0f, 0)));
+            yield return new WaitForSeconds(Random.Range(3f, 5f));
+        }
+    }
+}
