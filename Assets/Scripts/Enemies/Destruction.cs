bf219a358144bda514b0491b952712ea3825e567
Sardorbek <sardorbekmamadaliyevv@gmail.com>
2021-02-23 23:00:52 +0500

    fixed name error

---
+++ Assets/Scripts/Enemies/Destruction.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Destruction : Movement
+{
+    [SerializeField]
+    private float _destroyTime;
+
+    private void Start()
+    {
+        Destroy(gameObject, _destroyTime);
+    }
+}
