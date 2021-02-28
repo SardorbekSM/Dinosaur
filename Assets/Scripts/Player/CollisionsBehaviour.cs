700a2d4c5bd748d2ecc44681ee6d9864429e8ad9
Sardorbek <sardorbekmamadaliyevv@gmail.com>
2021-02-28 22:47:31 +0500

    changed the names of the script and removed the unnecessary function

---
+++ Assets/Scripts/Player/CollisionsBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CollisionsBehaviour : GameOverScene
+{ 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+            Hide();
+        if(collision.gameObject.CompareTag("Coin"))
+            Destroy(collision.gameObject);
+    }
+}
