29dc979fdb765f6b06b61dc5ddc5ca4e409998f0
Sardorbek <sardorbekmamadaliyevv@gmail.com>
2021-03-04 20:12:51 +0500

    added a new script for deleting objects

---
+++ Assets/Scripts/Enemies/AutoDestructor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class AutoDestructor : MonoBehaviour
+{
+    [SerializeField]
+    private float _destroyTime;
+
+    private void Start()
+    {
+        Destroy(gameObject, _destroyTime);
+    }
+}
