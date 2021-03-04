4f951ac708912dae66a3793cb5b28b7f285cabc0
Sardorbek <sardorbekmamadaliyevv@gmail.com>
2021-03-04 20:16:10 +0500

    added a new script to pause and save the scene

---
+++ Assets/Scripts/GameProcess/GameProcessPause.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameProcessPause : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+
+    public void PauseOrPlay()
+    {
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+        }
+        else
+            Time.timeScale = 1;
+    }
+}
