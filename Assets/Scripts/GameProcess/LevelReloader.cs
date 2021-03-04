05760b481a5a5ddfa6dd0abce8b90f9f2187bbc5
Sardorbek <sardorbekmamadaliyevv@gmail.com>
2021-03-04 20:02:39 +0500

    moved to another folder and deleted the timescale line changing to event

--- Assets/Scripts/GameProcess/LevelReloader.cs
+++ Assets/Scripts/GameProcess/LevelReloader.cs
@@ -1,10 +1,14 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 public class LevelReloader : MonoBehaviour
 {
+    [SerializeField]
+    private UnityEvent _notify;
+
     public void Reloaded()
     {
         Application.LoadLevel(Application.loadedLevel);
-        Time.timeScale = 1;
+        _notify?.Invoke();
     }
 }
