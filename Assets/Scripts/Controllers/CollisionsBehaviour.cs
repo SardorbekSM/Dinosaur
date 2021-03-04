88b83b511417fb8a337e69b85ae7c4bef45a5530
Sardorbek <sardorbekmamadaliyevv@gmail.com>
2021-03-04 20:08:03 +0500

    changed collision type in method

---
+++ Assets/Scripts/Controllers/CollisionsBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class CollisionsBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private EventManager notify;
+
+    [SerializeField]
+    private UnityEvent gameEndNotify;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            gameEndNotify?.Invoke();   
+        } 
+        if (collision.gameObject.CompareTag("Coin"))
+        {
+            notify.Invoke(collision.gameObject);
+        }
+    }
+}
