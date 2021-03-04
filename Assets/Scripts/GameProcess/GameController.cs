f1b6486c82fbbca910d2c823eec1d4b488e0f2ae
Sardorbek <sardorbekmamadaliyevv@gmail.com>
2021-03-04 19:58:14 +0500

    changed the name of the script and added a variable of the event type

---
+++ Assets/Scripts/GameProcess/GameController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent _notify;
+
+    private bool _active = false;
+
+    private void Awake()
+    {
+        Active(_active);
+    }
+
+    public void Hide()
+    {
+        Active(!_active);
+        _notify?.Invoke();
+    }
+
+    public bool Active(bool active)
+    {
+        gameObject.SetActive(active);
+        return !active;
+    }
+}
