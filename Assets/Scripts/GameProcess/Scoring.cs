a7043e9a5551a9df832fc446b97dbf2fb7522e06
Sardorbek <sardorbekmamadaliyevv@gmail.com>
2021-03-05 06:00:15 +0500

    removed unneeded spaces and type convertation

--- Assets/Scripts/GameProcess/Scoring.cs
+++ Assets/Scripts/GameProcess/Scoring.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using System;
 
 public class Scoring : MonoBehaviour
 {
     private int _score;
     private Text _scoreText;
+
+    [SerializeField]
     private int _calcAccuracy = 10;
 
     private void Awake()
@@ -16,6 +17,6 @@ public class Scoring : MonoBehaviour
     private void FixedUpdate()
     {
         ++_score;
-        _scoreText.text = Convert.ToString(_score/_calcAccuracy);
+        _scoreText.text = (_score / _calcAccuracy).ToString();
     }
 }
