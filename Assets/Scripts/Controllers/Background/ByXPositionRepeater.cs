85a4e95b1cb1a358852110185d239283f01e45dd
Sardorbek <sardorbekmamadaliyevv@gmail.com>
2021-03-05 06:01:21 +0500

    renamed the folder

---
+++ Assets/Scripts/Controllers/Background/ByXPositionRepeater.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ByXPositionRepeater : MonoBehaviour
+{
+    private float _size;
+
+    private void Start()
+    {
+        _size = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    private void Update()
+    {
+        Repeat();
+    }
+
+    private void Repeat()
+    {
+        transform.position = new Vector3(Mathf.Repeat(transform.position.x, _size), transform.position.y, transform.position.z);
+    }
+}
