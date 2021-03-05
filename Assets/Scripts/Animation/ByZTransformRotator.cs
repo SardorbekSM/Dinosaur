2ed29a4aebceeb93517351af348d52bdd2e5c1e0
Sardorbek <sardorbekmamadaliyevv@gmail.com>
2021-03-05 20:59:15 +0500

    commit deleted

--- Assets/Scripts/Animation/ByZTransformRotator.cs
+++ Assets/Scripts/Animation/ByZTransformRotator.cs
@@ -6,10 +6,10 @@ public class ByZTransformRotator : MonoBehaviour
 
     [SerializeField]
     private int _calcAccuracy = 100;
-    // Update is called once per frame
+
     private void FixedUpdate()
     {
         _rotate += Time.deltaTime * _calcAccuracy;
-        transform.Rotate(0,0, _rotate);        
+        transform.Rotate(0, 0, _rotate);
     }
 }
